f14e89e34628115be234c7bb95d76784cb609760
legacyguard <info@legacyguard.eu>
2025-08-11 15:34:29 +0200

    test(e2e): fix baseUrl port from 3000 to 4173 (vite preview port)

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:4173',
     supportFile: 'cypress/support/e2e.js',
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     video: false,
