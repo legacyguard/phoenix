5d22c99cb87a9ae4768c5458f16ec3e8336488c2
legacyguard <info@legacyguard.eu>
2025-08-12 21:44:07 +0200

    fixes

--- _dep_cleanup_20250811_124246/fallback.cjs
+++
@@ -1,4 +0,0 @@
-const d = require(process.argv[1]);
-console.log((d.deps || []).join("\n"));
-console.log("---DEV---");
-console.log((d.dev || []).join("\n"));
--- _pr_20250811_154003/get_base_url.cjs
+++
@@ -1,3 +0,0 @@
-let base = '';
-try { const cfg = require('./cypress.config.cjs'); base = (cfg?.e2e?.baseUrl || cfg?.baseUrl || ''); } catch {}
-process.stdout.write(base);
